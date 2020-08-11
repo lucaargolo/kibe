9a33cc9b84a8c14ccff265ddaa9ad72079356a6b
lucaargolo <lucaargolo@gmail.com>
2020-08-11 16:59:30 -0300

    Wall/Roof spikes (closes #17)

--- src/main/kotlin/io/github/lucaargolo/kibe/blocks/miscellaneous/Spikes.kt
+++ src/main/kotlin/io/github/lucaargolo/kibe/blocks/miscellaneous/Spikes.kt
@@ -1,27 +1,40 @@
 package io.github.lucaargolo.kibe.blocks.miscellaneous
 
 import io.github.lucaargolo.kibe.utils.FakePlayerEntity
-import net.minecraft.block.Block
-import net.minecraft.block.BlockRenderType
-import net.minecraft.block.BlockState
-import net.minecraft.block.ShapeContext
+import net.minecraft.block.*
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.damage.DamageSource
-import net.minecraft.server.world.ServerWorld
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.Properties
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 
 class Spikes(private val damage: Float, private val isPlayer: Boolean, settings: Settings): Block(settings) {
 
-    override fun onSteppedOn(world: World, pos: BlockPos, entity: Entity) {
-        if(world is ServerWorld) {
-            if(entity is LivingEntity) {
-                if(isPlayer) entity.damage(DamageSource.player(FakePlayerEntity(world)), damage)
-                else entity.damage(DamageSource.GENERIC, damage)
-            }
+
+    init {
+        defaultState = defaultState.with(Properties.FACING, Direction.UP)
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        builder.add(Properties.FACING)
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return defaultState.with(Properties.FACING, ctx.side)
+    }
+
+    override fun onEntityCollision(state: BlockState, world: World, pos: BlockPos, entity: Entity) {
+        if(!world.isClient && entity is LivingEntity) {
+            if(isPlayer)
+                entity.damage(DamageSource.player(FakePlayerEntity(world)), damage)
+            else
+                entity.damage(DamageSource.GENERIC, damage)
         }
     }
 
@@ -29,18 +42,23 @@ class Spikes(private val damage: Float, private val isPlayer: Boolean, settings:
         return BlockRenderType.MODEL
     }
 
-
     override fun hasSidedTransparency(state: BlockState?): Boolean {
         return true
     }
 
+    override fun getOutlineShape(state: BlockState, view: BlockView, pos: BlockPos, ePos: ShapeContext) = getShape(state[Properties.FACING])
 
-    override fun getOutlineShape(state: BlockState?, view: BlockView?, pos: BlockPos?, ePos: ShapeContext): VoxelShape {
-        return createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0)
-    }
+    override fun getCollisionShape(state: BlockState, view: BlockView, pos: BlockPos, ePos: ShapeContext) = getShape(state[Properties.FACING])
 
-    override fun getCollisionShape(state: BlockState?, view: BlockView?, pos: BlockPos?, ePos: ShapeContext): VoxelShape {
-        return createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0)
+    private fun getShape(facing: Direction): VoxelShape {
+        return when(facing) {
+            Direction.UP -> createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0)
+            Direction.DOWN -> createCuboidShape(0.0, 8.0, 0.0, 16.0, 16.0, 16.0)
+            Direction.EAST -> createCuboidShape(0.0, 0.0, 0.0, 8.0, 16.0, 16.0)
+            Direction.WEST -> createCuboidShape(8.0, 0.0, 0.0, 16.0, 16.0, 16.0)
+            Direction.SOUTH -> createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 8.0)
+            Direction.NORTH -> createCuboidShape(0.0, 0.0, 8.0, 16.0, 16.0, 16.0)
+        }
     }
 
 }
