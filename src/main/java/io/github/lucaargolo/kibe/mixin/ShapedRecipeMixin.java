3a23180ffa6421f9e8c552b365fd11df916c1727
lucaargolo <lucaargolo@gmail.com>
2020-08-24 16:55:05 -0300

    Fuck mirrored recipes. Bump version to 1.3.2

---
+++ src/main/java/io/github/lucaargolo/kibe/mixin/ShapedRecipeMixin.java
@@ -0,0 +1,25 @@
+package io.github.lucaargolo.kibe.mixin;
+
+import io.github.lucaargolo.kibe.items.ItemCompendiumKt;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.ShapedRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ShapedRecipe.class)
+public abstract class ShapedRecipeMixin {
+
+    @Shadow public abstract ItemStack getOutput();
+
+    @Inject(at = @At("HEAD"), method = "matchesSmall", cancellable = true)
+    private void matchesSmall(CraftingInventory inv, int offsetX, int offsetY, boolean bl, CallbackInfoReturnable<Boolean> info) {
+        if(getOutput().getItem() == ItemCompendiumKt.getGLIDER_LEFT_WING() || getOutput().getItem() == ItemCompendiumKt.getGLIDER_RIGHT_WING()) {
+            if(bl) info.setReturnValue(false);
+        }
+    }
+
+}
