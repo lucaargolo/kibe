d4b3c73d5b8dba287aecf43739afab9ec1dac449
lucaargolo <lucaargolo@gmail.com>
2021-01-18 18:59:07 -0300

    Blocks damage all damage received by the fake player (Solves a potential crash with Damage Tilt i guess)

--- src/main/kotlin/io/github/lucaargolo/kibe/utils/FakePlayerEntity.kt
+++ src/main/kotlin/io/github/lucaargolo/kibe/utils/FakePlayerEntity.kt
@@ -2,6 +2,7 @@ package io.github.lucaargolo.kibe.utils
 
 import com.mojang.authlib.GameProfile
 import io.github.lucaargolo.kibe.FAKE_PLAYER_UUID
+import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -10,5 +11,7 @@ class FakePlayerEntity(world: World): PlayerEntity(world, BlockPos.ORIGIN, 0f, G
 
     override fun isSpectator() = true
     override fun isCreative() = true
+
+    override fun damage(source: DamageSource?, amount: Float) = false
 
 }
