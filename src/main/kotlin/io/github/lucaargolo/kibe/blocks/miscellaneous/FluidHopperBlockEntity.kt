530b757902567d59fdcf7f942bec1155084ecdd3
lucaargolo <lucaargolo@gmail.com>
2021-10-12 17:06:26 -0300

    Bump insertion and extraction values every tick, so it works with cauldrons and other storages with fixed input/output

--- src/main/kotlin/io/github/lucaargolo/kibe/blocks/miscellaneous/FluidHopperBlockEntity.kt
+++ src/main/kotlin/io/github/lucaargolo/kibe/blocks/miscellaneous/FluidHopperBlockEntity.kt
@@ -20,6 +20,9 @@ import net.minecraft.world.World
 
 class FluidHopperBlockEntity(block: FluidHopper, pos: BlockPos, state: BlockState): BlockEntity(getEntityType(block), pos, state) {
 
+    var extractableBump = 1L
+    var insertableBump = 1L
+
     val tank = object: SingleVariantStorage<FluidVariant>() {
         override fun getBlankVariant(): FluidVariant = FluidVariant.blank()
         override fun getCapacity(variant: FluidVariant?): Long = FluidConstants.BUCKET
@@ -28,12 +31,16 @@ class FluidHopperBlockEntity(block: FluidHopper, pos: BlockPos, state: BlockStat
     override fun writeNbt(tag: NbtCompound): NbtCompound {
         super.writeNbt(tag)
         writeTank(tag, tank)
+        tag.putByte("extractableBump", extractableBump.toByte())
+        tag.putByte("insertableBump", insertableBump.toByte())
         return tag
     }
 
     override fun readNbt(tag: NbtCompound) {
         super.readNbt(tag)
         readTank(tag, tank)
+        extractableBump = tag.getByte("extractableBump").toLong()
+        insertableBump = tag.getByte("insertableBump").toLong()
     }
 
     companion object {
@@ -42,22 +49,32 @@ class FluidHopperBlockEntity(block: FluidHopper, pos: BlockPos, state: BlockStat
             val direction = state[HopperBlock.FACING]
 
             FluidStorage.SIDED.find(world, pos.up(), Direction.DOWN)?.let { extractable ->
-                if(entity.tank.isResourceBlank) {
-                    StorageUtil.move(extractable, entity.tank, { true }, 50*81, null)
+                val moved = if(entity.tank.isResourceBlank) {
+                    StorageUtil.move(extractable, entity.tank, { true }, (50*entity.extractableBump)*81, null)
                 }else {
                     if(entity.tank.amount <= 950*81) {
-                        StorageUtil.move(extractable, entity.tank, { it == entity.tank.variant }, 50*81, null)
+                        StorageUtil.move(extractable, entity.tank, { it == entity.tank.variant }, (50*entity.extractableBump)*81, null)
                     }else{
                         0
                     }
                 }
+                if(moved == 0L && entity.extractableBump < 20) {
+                    entity.extractableBump++
+                }else{
+                    entity.extractableBump = 1L
+                }
             }
 
             FluidStorage.SIDED.find(world, pos.add(direction.vector), direction.opposite)?.let { insertable ->
-                if(!entity.tank.isResourceBlank && entity.tank.amount >= 50*81) {
-                   StorageUtil.move(entity.tank, insertable, { true }, 50*81, null)
+                val moved = if(!entity.tank.isResourceBlank && entity.tank.amount >= 50*81) {
+                   StorageUtil.move(entity.tank, insertable, { true }, (50*entity.insertableBump)*81, null)
                 } else {
                     0
+                }
+                if(moved == 0L && entity.insertableBump < 20) {
+                    entity.insertableBump++
+                }else{
+                    entity.insertableBump = 1L
                 }
             }
 
