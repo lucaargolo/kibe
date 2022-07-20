16ebd4cb586b85734e84cd7720183181312e39d1
lucaargolo <lucaargolo@gmail.com>
2022-07-20 02:22:08 -0300

    Change fake light injection to make big torch work with Phosphor and Starlight (Closes #151, Closes #77)

--- src/main/java/io/github/lucaargolo/kibe/mixin/LightingProviderMixin.java
+++ src/main/java/io/github/lucaargolo/kibe/mixin/LightingProviderMixin.java
@@ -1,18 +1,19 @@
+
 package io.github.lucaargolo.kibe.mixin;
 
 import io.github.lucaargolo.kibe.blocks.bigtorch.BigTorchBlockEntity;
-import net.minecraft.world.chunk.light.BlockLightStorage;
-import net.minecraft.world.chunk.light.SkyLightStorage;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({SkyLightStorage.class, BlockLightStorage.class})
-public class LightStorageMixin {
+@Mixin(LightingProvider.class)
+public class LightingProviderMixin {
 
     @Inject(at = @At("HEAD"), method = "getLight", cancellable = true)
-    public void getLight(long blockPos, CallbackInfoReturnable<Integer> cir) {
+    public void getLight(BlockPos pos, int ambientDarkness, CallbackInfoReturnable<Integer> cir) {
         if(BigTorchBlockEntity.Companion.isTesting() && Thread.currentThread() == BigTorchBlockEntity.Companion.getTestingThread()) {
             cir.setReturnValue(15);
         }
