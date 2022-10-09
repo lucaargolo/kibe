74575d03cc8ac602e2986e429a97c81a5e9dc742
lucaargolo <lucaargolo@gmail.com>
2022-10-09 16:06:29 -0300

    Fixes dehumidifier (Closes #165)

--- src/main/java/io/github/lucaargolo/kibe/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/lucaargolo/kibe/mixin/WorldRendererMixin.java
@@ -1,26 +1,34 @@
 package io.github.lucaargolo.kibe.mixin;
 
+import io.github.lucaargolo.kibe.blocks.miscellaneous.DehumidifierBlockEntity;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 
-//    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getTopY(Lnet/minecraft/world/Heightmap$Type;II)I"), method = "renderWeather")
-//    private int onRenderWeather(ClientWorld clientWorld, Heightmap.Type heightmap, int x, int z) {
-//        if(DehumidifierBlockEntity.Companion.isBeingDehumidified(new ChunkPos(new BlockPos(x, 0, z)))) {
-//            return 512;
-//        }
-//        return clientWorld.getTopY(heightmap, x, z);
-//    }
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getTopY(Lnet/minecraft/world/Heightmap$Type;II)I"), method = "renderWeather")
+    private int onRenderWeather(World world, Heightmap.Type heightmap, int x, int z) {
+        if(DehumidifierBlockEntity.Companion.isBeingDehumidified(new ChunkPos(new BlockPos(x, 0, z)))) {
+            return 512;
+        }
+        return world.getTopY(heightmap, x, z);
+    }
 
-//    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getTopPosition(Lnet/minecraft/world/Heightmap$Type;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/BlockPos;"), method = "tickRainSplashing")
-//    private BlockPos onTickRainSplashing(ClientWorld clientWorld, Heightmap.Type heightmap, BlockPos pos) {
-//        if(DehumidifierBlockEntity.Companion.isBeingDehumidified(new ChunkPos(pos))) {
-//            return new BlockPos(pos.getX(), 512, pos.getZ());
-//        }
-//        return clientWorld.getTopPosition(heightmap, pos);
-//    }
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldView;getTopPosition(Lnet/minecraft/world/Heightmap$Type;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/BlockPos;"), method = "tickRainSplashing")
+    private BlockPos onTickRainSplashing(WorldView world, Heightmap.Type heightmap, BlockPos pos) {
+        if(DehumidifierBlockEntity.Companion.isBeingDehumidified(new ChunkPos(pos))) {
+            return new BlockPos(pos.getX(), 512, pos.getZ());
+        }
+        return world.getTopPosition(heightmap, pos);
+    }
 
 
 }
