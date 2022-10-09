da2b1206955be007865513597dab428c86eaa63a
lucaargolo <lucaargolo@gmail.com>
2022-10-09 14:46:27 -0300

    Fix Kibe lasso code being wrongly executed on the client which sometimes can cause desyncs (Closes #160)

--- src/main/java/io/github/lucaargolo/kibe/mixin/MobEntityMixin.java
+++ src/main/java/io/github/lucaargolo/kibe/mixin/MobEntityMixin.java
@@ -1,48 +1,40 @@
 package io.github.lucaargolo.kibe.mixin;
 
 import io.github.lucaargolo.kibe.effects.EffectCompendiumKt;
-import io.github.lucaargolo.kibe.items.miscellaneous.Lasso;
-import net.minecraft.entity.Entity;
+import io.github.lucaargolo.kibe.items.ItemCompendiumKt;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@SuppressWarnings("ConstantConditions")
 @Mixin(MobEntity.class)
-public class MobEntityMixin {
+public abstract class MobEntityMixin extends LivingEntity {
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Inject(at = @At("HEAD"), method = "cannotDespawn", cancellable = true)
     public void cannotDespawn(CallbackInfoReturnable<Boolean> info) {
-        boolean isCursed = ((MobEntity) ((Object) this)).hasStatusEffect(EffectCompendiumKt.getCURSED_EFFECT());
+        boolean isCursed = hasStatusEffect(EffectCompendiumKt.getCURSED_EFFECT());
         if(isCursed) info.setReturnValue(true);
     }
 
-    @Inject(at = @At("HEAD"), method = "interactWithItem", cancellable = true)
-    public void method_29506(PlayerEntity playerEntity, Hand hand, CallbackInfoReturnable<ActionResult> info) {
-        ItemStack stack = playerEntity.getStackInHand(hand);
-        Item item = stack.getItem();
-        if(item instanceof Lasso lasso) {
-            NbtCompound stackTag = stack.getNbt();
-            MobEntity entity = (MobEntity) ((Object) this);
-            if(stackTag != null && !stackTag.contains("Entity")) {
-                if (lasso.canStoreEntity(entity.getType())) {
-                    if(entity.isLeashed()) entity.detachLeash(true, true);
-                    entity.fallDistance = 0;
-                    NbtCompound tag = new NbtCompound();
-                    entity.saveSelfNbt(tag);
-                    stackTag.put("Entity", tag);
-                    stack.setNbt(stackTag);
-                    entity.remove(Entity.RemovalReason.DISCARDED);
-                    info.setReturnValue(ActionResult.SUCCESS);
-                }
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z", ordinal = 0), method = "interactWithItem", locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    public void method_29506(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir, ItemStack itemStack) {
+        if(itemStack.isOf(ItemCompendiumKt.getGOLDEN_LASSO()) || itemStack.isOf(ItemCompendiumKt.getCURSED_LASSO()) || itemStack.isOf(ItemCompendiumKt.getDIAMOND_LASSO())) {
+            ActionResult actionResult = itemStack.useOnEntity(player, this, hand);
+            if (actionResult.isAccepted()) {
+                cir.setReturnValue(actionResult);
             }
         }
     }
