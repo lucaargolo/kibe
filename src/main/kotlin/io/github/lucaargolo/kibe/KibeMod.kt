3bd8dc2ad6011423f65a1909da3966ec2b944732
lucaargolo <lucaargolo@gmail.com>
2025-09-23 17:44:25 -0300

    This isn't needed on Fabric but will be needed on Neoforge.

--- src/main/kotlin/io/github/lucaargolo/kibe/KibeMod.kt
+++ src/main/kotlin/io/github/lucaargolo/kibe/KibeMod.kt
@@ -9,6 +9,7 @@ import io.github.lucaargolo.kibe.data.state.ChunkLoaderState
 import io.github.lucaargolo.kibe.effect.EffectCompendium
 import io.github.lucaargolo.kibe.entity.EntityCompendium
 import io.github.lucaargolo.kibe.fluid.FluidCompendium
+import io.github.lucaargolo.kibe.item.ArmorMaterialCompendium
 import io.github.lucaargolo.kibe.item.ItemCompendium
 import io.github.lucaargolo.kibe.menu.ScreenHandlerCompendium
 import io.github.lucaargolo.kibe.network.PacketCompendium
@@ -91,6 +92,7 @@ object KibeMod : ModInitializer {
         CreativeTab.initialize()
         RecipeSerializerCompendium.initialize()
         RecipeTypeCompendium.initialize()
+        ArmorMaterialCompendium.initialize()
         FluidCompendium.initialize()
         BlockCompendium.initialize()
         ItemCompendium.initialize()
